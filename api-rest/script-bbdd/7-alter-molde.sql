20597b3577c8d1b78817202d1eb761467440a9e7
Avaco Sistemas <avacosistemas@gmail.com>
2022-09-14 07:35:35 -0300

    scripts molde

--- api-rest/script-bbdd/7-alter-molde.sql
+++ api-rest/script-bbdd/7-alter-molde.sql
@@ -4,4 +4,5 @@ ALTER TABLE public.moldeboca
 ALTER TABLE public.moldes
     ALTER COLUMN estado TYPE character varying(50);
 	
-alter table public.moldeboca add column descripcion  TYPE character varying(100);
+alter table public.moldeboca add column descripcion  character varying(100);
+
