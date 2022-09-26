5d56ab227e72bb22732a64516d995030fa987d35
Avaco Sistemas <avacosistemas@gmail.com>
2022-09-26 10:15:00 -0300

    se quitan proyectos que no se usan

--- api-rest/ws-rest-parameter/src/main/java/ar/com/avaco/service/ParameterEPService.java
+++
@@ -1,8 +0,0 @@
-package ar.com.avaco.service;
-
-import ar.com.avaco.ws.dto.ParameterDTO;
-import ar.com.avaco.ws.rest.service.CRUDEPService;
-
-public interface ParameterEPService extends CRUDEPService<Integer, ParameterDTO> {
-
-}
