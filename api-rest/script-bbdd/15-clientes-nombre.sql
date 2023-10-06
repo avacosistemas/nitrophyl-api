038c40857accab26df859b55a8a2a37c4baeed7c
Avaco Sistemas <avacosistemas@gmail.com>
2023-10-06 15:22:17 -0300

    sql

--- api-rest/script-bbdd/15-clientes-nombre.sql
+++ api-rest/script-bbdd/15-clientes-nombre.sql
@@ -1,7 +1,3 @@
 ALTER TABLE cliente ADD COLUMN nombre character varying(200);
-update cliente c set nombre = (select razonsocial from cliente c2 where id = c.id = c2.id);
-ALTER TABLE public.cliente ALTER COLUMN nombre SET NOT NULL;
-
-ALTER TABLE cliente ADD COLUMN nombre character varying(200);
 UPDATE cliente c SET nombre = (SELECT razon_social FROM cliente c2 WHERE id_cliente = c2.id_cliente);
 ALTER TABLE public.cliente ALTER COLUMN nombre SET NOT NULL;
